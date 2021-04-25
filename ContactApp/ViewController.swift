592dca9d6c44dcc8177343afd289c9c6d3b6d4de
syedmahvish <syedmahvish822@gmail.com>
2021-04-25 16:37:05 -0400

    contact app initial setup

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by Mahvish Syed on 25/04/21.
+//  Copyright © 2021 Mahvish Syed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
