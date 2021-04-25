0b0f08be51bf907b38909252b4f13b35060caf64
syedmahvish <syedmahvish822@gmail.com>
2021-04-25 17:53:44 -0400

    UI setup done

--- ContactApp/ViewController.swift
+++ ContactApp/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var profilePictureImageView: UIImageView!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var mobileTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func uploadProfilePicture(_ sender: UIButton) {
+    }
+    
+    
 }
 
